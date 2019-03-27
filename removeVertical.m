dbac987da62afc536ea192e4c4a79d5ca6b3a7d2
Kevin Singh <singhk7@yahoo.com>
2019-03-27 01:45:32 -0500

    min seam traceback fixed

--- removeVertical.m
+++ removeVertical.m
@@ -6,9 +6,12 @@ image = im2double(im);
 gray_image = rgb2gray(image);
 
 %% Compute the energy function at each pixel using the magnitude of the x and y gradients
-[fx, fy] = imgradientxy(gray_image, 'prewitt'); % equivalent to doing dF/dx and dF/dy
-%imshowpair(fx, fy);
-energy_map = sqrt((fx.^2 + fy.^2)); 
+%[gx, gy] = imgradientxy(gray_image); % equivalent to doing dF/dx and dF/dy
+[energy_map, gdir] = imgradient(gray_image);
+%imshowpair(gx, gy);
+%imshow(energy_map);
+%energy_map = sqrt((gx.^2 + gy.^2)); 
+%energy_map = abs(gx) + abs(gy);
 %imshow(energy_map);
 %imagesc(energy_map);
 
@@ -30,35 +33,55 @@ for j=1:cols   % iterate cols
     end
 end
 
+% for i=2:rows
+%    for j=1:cols
+%        if j==1
+%            M(i, j) = energy_map(i, j) + min([M(i-1, j), M(i-1, j+1)]);
+%            continue;
+%        elseif j==cols
+%            M(i, j) = energy_map(i, j) + min([M(i-1, j-1), M(i-1, j)]);
+%            continue;
+%        else
+%            M(i, j) = energy_map(i, j) + min([M(i-1, j-1), M(i-1, j), M(i-1, j+1)]);
+%        end
+%    end
+% end
+
 %imshow(M);
 
 %% get min seam in energy map 
 new_image = zeros(size(image));
 new_image(:) = image(:);
-used = [];
-for i=1:numPixels
-    minPx = M(rows, 1);
-    minCol = 1;
-    %[minPx, minCol] = min(M(rows,:));  %NOT WORKING
-    for j=1:cols     % find the min value in last row of M
-        if(M(rows, j) < minPx)
-            %minPx = (M(rows, j));
-            %minCol = j;
-            [minPx, minCol] = min(M(rows,:));
-%             if ismember(minCol, used)
-%                 minCol
-%                 continue;
-%             end
-            %used = [used, minCol];
-            M(rows, minCol) = 1.00;    % take that px out of consideration
-        end
-    end
-    
+for i=1:numPixels   % decrease the image by the given # of pixels
+%     minPx = M(rows, 1);
+%     minCol = 1;
+%     for j=1:cols     % find the min value in last row of M
+%         if(M(rows, j) < minPx)
+%             %minPx = (M(rows, j));
+%             %minCol = j;            
+%             [minPx, minCol] = min(M(rows,:));
+%             M(rows, minCol) = 1.00;    % take that px out of consideration
+%         end
+%     end
+    [minPx, minCol] = min(M(rows,:));
+    M(rows, minCol) = 1.0;    % take that px out of consideration
     % trace back the min seam to remove
     for k=rows-1:-1:1
-        if(minCol > 1 && minCol < cols && M(k, minCol+1) < M(k, minCol-1) && M(k, minCol+1) < M(k, minCol))  % check if top right px is the minimum one
+        if(minCol == 1 && M(k, minCol) < M(k, minCol+1))
+            minCol = minCol;
+            continue;
+        elseif(minCol == 1 && M(k, minCol+1) < M(k, minCol))
             minCol = minCol+1;
-        elseif(minCol > 1 && minCol < cols && M(k, minCol-1) < M(k, minCol) && M(k, minCol-1) < M(k, minCol+1)) % check if top left px is min one
+            continue;
+        elseif(minCol == cols && M(k, minCol) < M(k, minCol-1))
+            minCol = minCol;
+            continue;
+        elseif(minCol == cols && M(k, minCol-1) < M(k, minCol))
+            minCol = minCol-1;
+            continue;
+        elseif(M(k, minCol+1) < M(k, minCol-1) && M(k, minCol+1) < M(k, minCol))  % check if top right px is the minimum one
+            minCol = minCol+1;
+        elseif(M(k, minCol-1) < M(k, minCol) && M(k, minCol-1) < M(k, minCol+1)) % check if top left px is min one
             minCol = minCol-1;
         else % top px is the min one
             minCol = minCol;
@@ -68,7 +91,7 @@ for i=1:numPixels
     end
 end
 
-imshow(M);
+imshow(new_image);
 
 output_image = M;
 
